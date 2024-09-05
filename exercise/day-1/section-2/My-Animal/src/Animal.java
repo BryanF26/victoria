89aff0b1864d34b5c6b38d3c6a4e246256f56ee0
10lalala <bryanfelix2001@gmail.com>
2024-09-05 16:25:03 +0700

    Overidding Animal

--- exercise/day-1/section-2/My-Animal/src/Animal.java
+++ exercise/day-1/section-2/My-Animal/src/Animal.java
@@ -1,4 +1,9 @@
 public class Animal {
+
+    void makeSound(){
+        System.out.println("Some generic animal sound");
+    }
+
     void eat(){
         System.out.println("This animal eats food.");
     }
