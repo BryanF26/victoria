ffce2c4a8c65f2803b0b86ce35e353f1939e69ab
10lalala <bryanfelix2001@gmail.com>
2024-09-05 10:45:14 +0700

    Luas Lingkaran

--- pre-test/day-1/Hello World/src/Main.java
+++ pre-test/day-1/Hello World/src/Main.java
@@ -1,24 +1,40 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
 
-        long bankAccount;
-        int myAge;
-        double temperature;
-        float temperatures;
-        char firstNameLetter;
-        boolean passTest;
-        bankAccount = 40000000;
-        myAge = 22;
-        temperature = 36.77;
-        temperatures = 36.77f;
-        firstNameLetter = 'B';
-        passTest = true;
-        System.out.println(bankAccount);
-        System.out.println(myAge);
-        System.out.println(temperature);
-        System.out.println(temperatures);
-        System.out.println(firstNameLetter);
-        System.out.println(passTest);
+    public static double LuasLingkaran(double pi, int r){
+        return (pi * Math.pow(r,2));
+    }
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//
+//        long bankAccount;
+//        int myAge;
+//        double temperature;
+//        float temperatures;
+//        char firstNameLetter;
+//        boolean passTest;
+//        bankAccount = 40000000;
+//        myAge = 22;
+//        temperature = 36.77;
+//        temperatures = 36.77f;
+//        firstNameLetter = 'B';
+//        passTest = true;
+//        System.out.println(bankAccount);
+//        System.out.println(myAge);
+//        System.out.println(temperature);
+//        System.out.println(temperatures);
+//        System.out.println(firstNameLetter);
+//        System.out.println(passTest);
+
+        final double pi = Math.PI;
+        int r;
+        r = 10;
+        double L1;
+        L1 = LuasLingkaran(pi, r);
+        r = 15;
+        double L2;
+        L2 = LuasLingkaran(pi, r);
+        System.out.println("Luas lingkaran 1 adalah "+L1);
+        System.out.println("Luas lingkaran 2 adalah "+L2);
     }
 }
