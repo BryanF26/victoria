94a3a383518bb28a399fb0a7927fb3d5c7ea5379
10lalala <bryanfelix2001@gmail.com>
2024-09-05 14:47:06 +0700

    Constructor Motor Sepeda

--- exercise/day-1/section-2/Car/src/Main.java
+++ exercise/day-1/section-2/Car/src/Main.java
@@ -5,5 +5,15 @@ public class Main {
         myCar.model = "Toyota";
         myCar.year = 2020;
         myCar.drive();
+
+        Motor myMotor = new Motor("Honda", 2020);
+        System.out.println(myMotor.model+" "+myMotor.year);
+        Motor myMotor2 = new Motor("Yamaha", 2022);
+        System.out.println(myMotor2.model+" "+myMotor2.year);
+
+        Sepeda mySepeda = new Sepeda();
+        System.out.println(mySepeda.model+" "+mySepeda.year);
+        Sepeda mySepeda2 = new Sepeda();
+        System.out.println(mySepeda2.model+" "+mySepeda2.year);
     }
 }
