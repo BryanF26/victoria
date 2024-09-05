92a5501559425ca5e09a700f9a30230b293b2639
10lalala <bryanfelix2001@gmail.com>
2024-09-05 10:31:47 +0700

    Primitive Data Type

--- pre-test/day-1/Hello World/src/Main.java
+++ pre-test/day-1/Hello World/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        long bankAccount;
+        int myAge;
+        double temperature;
+        float temperatures;
+        char firstNameLetter;
+        boolean passTest;
+        bankAccount = 40000000;
+        myAge = 22;
+        temperature = 36.77;
+        temperatures = 36.77f;
+        firstNameLetter = 'B';
+        passTest = true;
+        System.out.println(bankAccount);
+        System.out.println(myAge);
+        System.out.println(temperature);
+        System.out.println(temperatures);
+        System.out.println(firstNameLetter);
+        System.out.println(passTest);
     }
 }
