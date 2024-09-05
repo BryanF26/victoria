0750836ded087e7231a14240efb50d4da11c0f0c
10lalala <bryanfelix2001@gmail.com>
2024-09-05 14:11:05 +0700

    Class Object Car

--- pre-test/day-1/section-2/ObjectOrientedProgramming/src/Main.java
+++ pre-test/day-1/section-2/ObjectOrientedProgramming/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
+        Car myCar = new Car();
+        myCar.model = "Toyota";
+        myCar.year = 2020;
+        myCar.drive();
     }
 }
