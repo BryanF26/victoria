ba09993a4e50a383759b34e58bb53d38f8d7e09c
10lalala <bryanfelix2001@gmail.com>
2024-09-06 14:51:43 +0700

    Add panjang lebar dan pages sizes

--- exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
+++ exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
@@ -3,10 +3,7 @@ package com.example.first_application;
 import com.example.first_application.response.GetAssetResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -58,4 +55,44 @@ public class FirstApplication {
 	public List<String> getUser(){
 		return List.of("John", "Jane", "Jack");
 	}
+
+	@GetMapping("/calculator/add")
+	public String getAdd(
+		@RequestParam(name = "panjang") int panjang,
+		@RequestParam(name = "lebar", required = false, defaultValue = "20") int lebar
+	){
+		return String.valueOf(panjang+lebar);
+	}
+
+	@GetMapping("/assets/pages")
+	public ArrayList<GetAssetResponse> getAssetsByPageSize(
+		@RequestParam(name = "page") int page,
+		@RequestParam(name = "size") int size
+	){
+		ArrayList<GetAssetResponse> list = new ArrayList<>();
+		list.add(GetAssetResponse.builder().id(1L).fullName("Bryan Felix").age(12).gender("Male").build());
+		list.add(GetAssetResponse.builder().id(2L).fullName("Felice Isna").age(25).gender("Female").build());
+		list.add(GetAssetResponse.builder().id(3L).fullName("Ospdi Demo").age(23).gender("Female").build());
+		list.add(GetAssetResponse.builder().id(4L).fullName("Hsjdk Ihkd").age(21).gender("Male").build());
+		list.add(GetAssetResponse.builder().id(5L).fullName("Pasdj Iush").age(20).gender("Female").build());
+		list.add(GetAssetResponse.builder().id(6L).fullName("Pasdj Iush").age(20).gender("Female").build());
+		list.add(GetAssetResponse.builder().id(7L).fullName("Pasdj Iush").age(20).gender("Female").build());
+		list.add(GetAssetResponse.builder().id(8L).fullName("Pasdj Iush").age(20).gender("Female").build());
+		list.add(GetAssetResponse.builder().id(9L).fullName("Pasdj Iush").age(20).gender("Female").build());
+		list.add(GetAssetResponse.builder().id(10L).fullName("Pasdj Iush").age(20).gender("Female").build());
+
+		int temp = ((page-1) * size);
+		ArrayList<GetAssetResponse> tampung = new ArrayList<>();
+		if(list.size()-1 < temp){
+			return tampung;
+		}
+		for (int i=temp;i<temp+size;i++){
+			tampung.add(list.get(i));
+			if(list.size()-1 < i+1){
+				break;
+			}
+		}
+		return tampung;
+	}
+
 }
