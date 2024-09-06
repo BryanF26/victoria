4da6a556e8974ce743458279a2d382b62666a4cb
10lalala <bryanfelix2001@gmail.com>
2024-09-06 13:13:39 +0700

    All Users

--- exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
+++ exercise/day-2/section-1/first-application/src/main/java/com/example/first_application/FirstApplication.java
@@ -1,9 +1,14 @@
 package com.example.first_application;
 
+import com.example.first_application.response.GetAssetResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController // mendefinisikan class controller
 @SpringBootApplication
@@ -16,5 +21,21 @@ public class FirstApplication {
 	@GetMapping
 	public String sayHello(){
 		return "Hello World";
+	}
+
+	@GetMapping("/allUsers")
+	public ArrayList<GetAssetResponse> getAsset(){
+		ArrayList<GetAssetResponse> list = new ArrayList<>();
+		GetAssetResponse response = GetAssetResponse.builder().id(1L).fullName("Bryan Felix").age(12).gender("Male").build();
+		GetAssetResponse response2 = GetAssetResponse.builder().id(2L).fullName("Felice Isna").age(23).gender("Female").build();
+		list.add(response);
+		list.add(response2);
+		return list;
+	}
+
+
+	@GetMapping("/users")
+	public List<String> getUser(){
+		return List.of("John", "Jane", "Jack");
 	}
 }
